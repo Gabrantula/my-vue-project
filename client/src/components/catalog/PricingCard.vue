b2519fa7af362c0cc7ca1702ecccd85bfb09e0f9
Gabrantula <104615619+Gabrantula@users.noreply.github.com>
2023-12-09 09:17:21 +0200

    Update PricingCard.vue

--- client/src/components/catalog/PricingCard.vue
+++ client/src/components/catalog/PricingCard.vue
@@ -1,5 +1,11 @@
 
 <script>
+import { mapState } from 'pinia';
+
+import { useCartStore } from '../../pinia-store/cartStore';
+import { useUserStore } from '../../pinia-store/userStore';
+
+
 export default {
     props: {
         card: {
@@ -17,40 +23,52 @@ export default {
                 _id: "-1",
             })
         }
-    }
+    },
+    emits: ['onAddToCart'],
+    computed: {
+        ...mapState(useCartStore, ['getOffer']),
+        ...mapState(useUserStore, ['isAuth']),
+    },
+
 }
 </script>
 <template>
-    <li class="w3-black w3-xlarge w3-padding-32">
-        {{ card.title }}
-    </li>
-    <li class="w3-padding-16">
-        <b>{{ card.storage }}</b> Storage
-    </li>
-    <li class="w3-padding-16">
-        <b>{{ carrd.emails }}</b> Emails
-    </li>
-    <li class="w3-padding-16">
-        <b>{{ card.domains }}</b> Domains
-    </li>
-    <li class="w3-padding-16">
-        <b>{{ card.support }}</b> Support
-    </li>
-    <li class="w3-padding-16">
-        <h2 class="w3-wide">
-            $ {{ card.price }}
-        </h2>
-        <span class="w3-opacity">per month</span>
-    </li>
-    <li class="w3-light-grey w3-padding-24">
-        <!--  <button class="w3-button w3-black w3-padding-large">Sign Up</button> -->
-        <router-link v-if="isAuth" to="/cart" class="w3-button w3-black w3-padding-large">
-            Buy now
-        </router-link>
-        <router-link v-if="!isAuth" to="/login" class="w3-button w3-black w3-padding-large">
-            Buy now
-        </router-link>
-    </li>
+    <div class="w3-third w3-section">
+
+        <ul class="w3-ul w3-white w3-hover-shadow">
+            <li class="w3-black w3-xlarge w3-padding-32">
+                {{ card.title }}
+            </li>
+            <li class="w3-padding-16">
+                <b>{{ card.storage }}</b> Storage
+            </li>
+            <li class="w3-padding-16">
+                <b>{{ card.emails }}</b> Emails
+            </li>
+            <li class="w3-padding-16">
+                <b>{{ card.domains }}</b> Domains
+            </li>
+            <li class="w3-padding-16">
+                <b>{{ card.support }}</b> Support
+            </li>
+            <li class="w3-padding-16">
+                <h2 class="w3-wide">
+                    $ {{ card.price }}
+                </h2>
+                <span class="w3-opacity">per month</span>
+            </li>
+            <li class="w3-light-grey w3-padding-24">
+                <!--  <button class="w3-button w3-black w3-padding-large">Sign Up</button> -->
+                <router-link v-if="isAuth" to="/cart" @click="$emit('onAddToCart', card._id)"
+                    class="w3-button w3-black w3-padding-large">
+                    Buy now
+                </router-link>
+                <router-link v-if="!isAuth" to="/login" class="w3-button w3-black w3-padding-large">
+                    Buy now
+                </router-link>
+            </li>
+        </ul>
+    </div>
 </template>
 
 
