adb746b13a60a5ba900c22c0d4a50179d3747acd
Gabrantula <104615619+Gabrantula@users.noreply.github.com>
2023-12-09 09:22:58 +0200

    Create Person-contact.vue

---
+++ client/src/components/team/Person-contact.vue
@@ -0,0 +1,32 @@
+
+<script>
+import PersonCard from './PersonCard.vue';
+
+
+
+export default {
+    props: {
+        person: {}
+    },
+    components: { PersonCard }
+}
+</script>
+<template>
+    <div class="box">
+        <div class="image">
+            <PersonCard />
+        </div>
+        <div class="info">
+
+        </div>
+    </div>
+</template>
+
+
+
+<style scoped>
+.box {
+    width: 70vw;
+    height: 50vh;
+}
+</style>
