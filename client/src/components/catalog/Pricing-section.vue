1f5f536221187186fd28ef00aa5fa3d0217f1e46
Gabrantula <104615619+Gabrantula@users.noreply.github.com>
2023-12-09 09:16:07 +0200

    Update Pricing-section.vue

--- client/src/components/catalog/Pricing-section.vue
+++ client/src/components/catalog/Pricing-section.vue
@@ -7,6 +7,31 @@ import PricingCard from './pricingCard.vue';
 import { useCartStore } from '../../pinia-store/cartStore';
 
 export default {
+    /* components: { Loader, PricingCard },
+     data() {
+         return {
+             cards: [],
+             isLoading: true
+         }
+     },
+     computed: {
+         ...mapState(useUserStore, ['isAuth']),
+         ...mapState(useCartStore, ['getOffer']),
+ 
+         displayProducts() {
+             return this.cards
+         }
+     },
+     async created() {
+         const promis = await Promise.all([getAllProducts(), []])
+        
+         this.cards = promis[0]
+       
+         this.isLoading = false
+     },
+     methods: {
+         ...mapActions(useCartStore, ['addToCart'])
+     }*/
     components: { Loader, PricingCard },
     data() {
         return {
@@ -16,7 +41,7 @@ export default {
     },
     computed: {
         ...mapState(useUserStore, ['isAuth']),
-        ...mapState(useCartStore, ['getOffer']),
+        // ...mapState(useCartStore, ['getOffer']),
 
         displayProducts() {
             return this.cards
@@ -24,21 +49,30 @@ export default {
     },
     async created() {
         const promis = await Promise.all([getAllProducts(), []])
-        // this.products = promis[0].products
+
         this.cards = promis[0]
-        // console.log(this.cards);
-        // console.log(promis);
+
         this.isLoading = false
     },
     methods: {
         ...mapActions(useCartStore, ['addToCart'])
     }
+
 };
 </script>
 
 <template>
     <!-- Pricing Section -->
     <!-- <div class="w3-container w3-center w3-dark-grey" style="padding:128px 16px" id="pricing"> -->
+
+    <!--  <div id="pricing" class="w3-container w3-center pricing" style="padding:128px 16px">
+        <h3>PRICING</h3>
+        <p class="w3-large">
+            Choose a pricing plan that fits your needs.
+        </p>
+        <Loader v-if="isLoading" />
+        <div v-else class="w3-row-padding" style="margin-top:64px">
+            -->
     <div id="pricing" class="w3-container w3-center pricing" style="padding:128px 16px">
         <h3>PRICING</h3>
         <p class="w3-large">
@@ -46,7 +80,10 @@ export default {
         </p>
         <Loader v-if="isLoading" />
         <div v-else class="w3-row-padding" style="margin-top:64px">
-            <!--  
+
+
+
+            <!--  tova e staro trite diva
             <div class="w3-third w3-section">
                 <ul class="w3-ul w3-white w3-hover-shadow">
                     <li class="w3-black w3-xlarge w3-padding-32">
@@ -148,7 +185,52 @@ export default {
                 </ul>
             </div>
 -->
-            <div v-for="card in displayProducts" :key="card._id" :card="card" class="w3-third w3-section">
+
+
+            <PricingCard v-for="card in displayProducts" :key="card._id" :card="card" @on-add-to-cart="addToCart">
+                <!--
+    <ul class="w3-ul w3-white w3-hover-shadow">
+                    <li class="w3-black w3-xlarge w3-padding-32">
+                        {{ card.title }}
+                    </li>
+                    <li class="w3-padding-16">
+                        <b>{{ card.storage }}</b> Storage
+                    </li>
+                    <li class="w3-padding-16">
+                        <b>{{ card.emails }}</b> Emails
+                    </li>
+                    <li class="w3-padding-16">
+                        <b>{{ card.domains }}</b> Domains
+                    </li>
+                    <li class="w3-padding-16">
+                        <b>{{ card.support }}</b> Support
+                    </li>
+                    <li class="w3-padding-16">
+                        <h2 class="w3-wide">
+                            $ {{ card.price }}
+                        </h2>
+                        <span class="w3-opacity">per month</span>
+                    </li>
+                    <li class="w3-light-grey w3-padding-24">
+
+                        <router-link v-if="isAuth" to="/cart" class="w3-button w3-black w3-padding-large">
+                            Buy now
+                        </router-link>
+                        <router-link v-if="!isAuth" to="/login" class="w3-button w3-black w3-padding-large">
+                            Buy now
+                        </router-link>
+                    </li>
+                </ul>
+-->
+
+            </PricingCard>
+        </div>
+    </div>
+
+
+    <!--  
+    <div v-for="card in displayProducts" :key="card._id" :card="card" @click="(addToCart, card._id)"
+                class="w3-third w3-section">
 
                 <ul class="w3-ul w3-white w3-hover-shadow">
                     <li class="w3-black w3-xlarge w3-padding-32">
@@ -173,9 +255,8 @@ export default {
                         <span class="w3-opacity">per month</span>
                     </li>
                     <li class="w3-light-grey w3-padding-24">
-                        <!--  <button class="w3-button w3-black w3-padding-large">Sign Up</button> -->
-                        <router-link v-if="isAuth" to="/cart" @click="addToCart"
-                            class="w3-button w3-black w3-padding-large">
+                       
+                        <router-link v-if="isAuth" to="/cart" class="w3-button w3-black w3-padding-large">
                             Buy now
                         </router-link>
                         <router-link v-if="!isAuth" to="/login" class="w3-button w3-black w3-padding-large">
@@ -186,6 +267,7 @@ export default {
             </div>
         </div>
     </div>
+-->
 </template>
 
 <style  scoped></style>
